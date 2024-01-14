756c79787f907dba367dde5e741bcc6d44a12a0c
Ellie Frost <web@stillinbeta.com>
2024-01-14 15:04:13 -0800

    cohost updater uses json now

--- updater/src/cohost.rs
+++ updater/src/cohost.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use rss::{Channel, Item};
 use serde::{Deserialize, Serialize};
 use slog::debug;
 
@@ -7,8 +6,26 @@ pub struct Cohost<'a> {
     log: &'a slog::Logger,
 }
 
+#[derive(Deserialize)]
+struct Page {
+    items: Vec<Item>
+}
+
+#[derive(Deserialize)]
+struct Item {
+    url: String,
+    title: String,
+    author: Author,
+}
+
+#[derive(Deserialize)]
+struct Author {
+    name: String,
+}
+
+
 impl<'a> Cohost<'a> {
-    const USER_RSS_FEED: &'static str = "https://cohost.org/stillinbeta/rss/public.rss";
+    const USER_PAGE_JSON: &'static str = "https://cohost.org/stillinbeta/rss/public.json";
     const AUTHOR_NAME: &'static str = "@stillinbeta";
 
     pub fn new(log: &'a slog::Logger) -> Self {
@@ -22,32 +39,27 @@ pub struct Chost {
     pub url: String,
 }
 
+impl Cohost<'_> {
+    fn get_page(&mut self) -> Result<Page> {
+        let client = reqwest::blocking::Client::new();
+        Ok(client
+            .get(Self::USER_PAGE_JSON)
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+}
+
 impl<'a> crate::Updater for Cohost<'a> {
     fn name(&self) -> &'static str {
         "cohost"
     }
 
+
     fn new_value(&mut self) -> Result<String> {
-        let client = reqwest::blocking::Client::new();
-        let feed = client
-            .get(Self::USER_RSS_FEED)
-            .send()?
-            .error_for_status()?
-            .bytes()?;
+        let page = self.get_page()?;
 
-        let channel = Channel::read_from(&feed[..])?;
-        let Item { title, link, .. } = channel
-            .items
-            .into_iter()
-            // find one we wrote
-            .find(|v| match &v.author {
-                Some(v) => v.contains(Self::AUTHOR_NAME),
-                None => false,
-            })
-            .unwrap();
-
-        let title = title.clone().unwrap_or("this one".into());
-        let url = link.clone().unwrap();
+        let Item { title, url, .. } = page.items.into_iter().find(|v| v.author.name == Self::AUTHOR_NAME).unwrap();
 
         debug!(self.log, "retrieved chost"; "title" => &title, "url" => &url);
 
